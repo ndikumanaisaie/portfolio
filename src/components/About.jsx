561f2ad362eff88f8c5a6fb83e9d34025b80a6ce
ndikumanaisaie <ndikumanaisaie@gmail.com>
2023-04-25 16:57:36 +0200

    Add slide reveal dependency

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,49 +1,52 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import { Box, Typography, Link, Stack } from '@mui/material';
+import Slide from 'react-reveal/Slide';
 import resume from '../images/resume.pdf';
 
 const About = () => (
-	<Box id="about">
-		<Typography
-				variant="h3"
-				mt="20px"
-				mb="20px"
-				color="#ddd"
-			>About Me</Typography>
-			<Box
-				gap="40px"
-				fontSize="24px"
-				alignItems="center"
-				marginTop="0.2rem"
-				className="about-me"
-			>
-				<Typography
-					mb={2}
+	<Slide bottom>
+		<Box id="about">
+			<Typography
+					variant="h3"
+					mt="20px"
+					mb="20px"
 					color="#ddd"
-					textAlign="justify"
-					fontSize="22px"
-					sx={{ width: { lg: '60%', xs: '100%' } }}
+				>About Me</Typography>
+				<Box
+					gap="40px"
+					fontSize="24px"
+					alignItems="center"
+					marginTop="0.2rem"
+					className="about-me"
 				>
-					I am a solution-oriented Front-End Developer,
-					lifelong learner, and adventure with a background in electronics.
-					I enjoy development because of the satisfaction I get by overcoming challenges.
-					I am motivated by the opportunity that web development
-					provides to positively impact individual life,
-					family, and the world as a whole. I am open to new opportunities.
-				</Typography>
-				<Link
-					className="download"
-					target="_blank"
-					href={resume}
-					type="application/octet-stream"
-					download="Ndikumana Isaie resume"
-					style={{ textDecoration: 'none' }}
-				>
-					Get my Resume
-				</Link>
-			</Box>
-	</Box>
+					<Typography
+						mb={2}
+						color="#ddd"
+						textAlign="justify"
+						fontSize="22px"
+						sx={{ width: { lg: '60%', xs: '100%' } }}
+					>
+						I am a solution-oriented Front-End Developer,
+						lifelong learner, and adventure with a background in electronics.
+						I enjoy development because of the satisfaction I get by overcoming challenges.
+						I am motivated by the opportunity that web development
+						provides to positively impact individual life,
+						family, and the world as a whole. I am open to new opportunities.
+					</Typography>
+					<Link
+						className="download"
+						target="_blank"
+						href={resume}
+						type="application/octet-stream"
+						download="Ndikumana Isaie resume"
+						style={{ textDecoration: 'none' }}
+					>
+						Get my Resume
+					</Link>
+				</Box>
+		</Box>
+	</Slide>
 );
 
 export default About;
