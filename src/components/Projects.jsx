32c27b09fa980691ddae970375a27152feccfe8b
ndikumanaisaie <ndikumanaisaie@gmail.com>
2023-05-01 10:57:41 +0200

    Fix indentation error

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -23,52 +23,52 @@ const Projects = () => {
 
 	return (
 		// <Slide direction="up" fraction={1} triggerOnce={true}>
-			<Box
-				id="projects"
-				mt="30px"
+		<Box
+			id="projects"
+			mt="30px"
+		>
+			<Typography
+				variant="h3"
+				mb="46px"
+				color="#444f5a"
 			>
-				<Typography
-					variant="h3"
-					mb="46px"
-					color="#444f5a"
-				>
 					Projects
-				</Typography>
+			</Typography>
 
-				<Box
-					className="projects-container"
-				>
-					{
-						currentProjects.map((project, i) => (
-							<ProjectCard
-								key={i}
-								project={project}
-								timeLineData={timeLineData}
-							/>
-						))
-					}
-				</Box>
-				<Stack
-					mt="100px"
-					alignItems="center"
-				>
-					{
-						projects.length > 4 && (
-							<Pagination
-								shape="rounded"
-								defaultPage={1}
-								count={Math.ceil(projects.length / projectPerPage)}
-								page = {currentPage}
-								onChange= {paginate}
-								size="large"
-								sx={{
-									color: '#0564bd', backgroundColor: '#3d90e3', borderRadius: '10px',
-								}}
-							/>
-						)
-					}
-				</Stack>
+			<Box
+				className="projects-container"
+			>
+				{
+					currentProjects.map((project, i) => (
+						<ProjectCard
+							key={i}
+							project={project}
+							timeLineData={timeLineData}
+						/>
+					))
+				}
 			</Box>
+			<Stack
+				mt="100px"
+				alignItems="center"
+			>
+				{
+					projects.length > 4 && (
+						<Pagination
+							shape="rounded"
+							defaultPage={1}
+							count={Math.ceil(projects.length / projectPerPage)}
+							page = {currentPage}
+							onChange= {paginate}
+							size="large"
+							sx={{
+								color: '#0564bd', backgroundColor: '#3d90e3', borderRadius: '10px',
+							}}
+						/>
+					)
+				}
+			</Stack>
+		</Box>
 		// </Slide>
 	);
 };
